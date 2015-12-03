58a49f03832c609d55c9278974d5648940dc4460
= <=>
2015-12-03 14:16:24 +0200

    add checkout controller

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Category;
+use App\Cart;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+
+}
