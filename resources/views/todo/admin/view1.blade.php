3c38c3cafa5c3bc0ebb209aef41174f205865d81
= <=>
2015-12-08 09:34:42 +0200

    remove todo

--- resources/views/todo/admin/view1.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('todo.user.main')
-@section('tabs')
-    <li class="active"><a href="http://users.metropolia.fi/~aleksniv/awsomeWebstore/admin/view1.html">Home</a></li>
-	<li><a href="http://users.metropolia.fi/~aleksniv/awsomeWebstore/admin/view2.html">Lisää tuote</a></li>
-	<li><a href="http://users.metropolia.fi/~aleksniv/awsomeWebstore/admin/view3.html">Tuotteiden hallinta</a></li>
-@endsection
-@section('content')
-	
-@endsection
