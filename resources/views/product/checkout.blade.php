2c1489b6a183d56b775979a011a743cb13e502fd
= <=>
2015-12-08 10:28:14 +0200

    slight view fixes

--- resources/views/product/checkout.blade.php
+++ resources/views/product/checkout.blade.php
@@ -31,8 +31,8 @@
 		<form role="form" action="/create-order" method="post">
 			{!! csrf_field() !!}
 			@foreach($cart as $cartItem)
-				<input type="hidden" name="product[]" value="{{$cart['product']->productid}}">
-				<input type="hidden" name="count[]" value="{{$cart['count']}}">
+				<input type="hidden" name="product[]" value="{{$cartItem['product']->productid}}">
+				<input type="hidden" name="count[]" value="{{$cartItem['count']}}">
 			@endforeach
 			<div class="form-group">
 		  		<label for="firstName">First name</label>
