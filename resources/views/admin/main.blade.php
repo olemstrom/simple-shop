5b0fb96b6149349f84b62853e9c862b161c01ac0
= <=>
2015-12-08 09:35:34 +0200

    stuff

---
+++ resources/views/admin/main.blade.php
@@ -0,0 +1,44 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  		<link rel="stylesheet" href="/styles/styles.css">
+		<title>Awsome Webstore</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+		  <div class="navigation">
+		    <div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			     	<span class="icon-bar"></span>                        
+			    </button>
+		      	<a class="navbar-brand" href="#">Awsome Webstore</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      	<ul class="nav navbar-nav">
+			        <li><a href="/admin">Home</a></li>
+					<li><a href="/admin/add-product">Add product</a></li>
+					<li><a href="/admin/add-category">Add category</a></li>
+					<li><a href="/admin/modify-products">Manage products</a></li>
+					<li><a href="/admin/modify-categories">Manage categories</a></li>
+					<li><a href="/admin/manage-orders">Manage orders</a></li>
+		      	</ul>
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li><a href="/auth/logout"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+			    </ul>
+		    </div>
+		  </div>
+		</nav>
+
+		<div class="container">
+			@yield('content')
+		</div>
+
+	@yield('scripts')
+	</body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,61 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  		<script>
+  			$(document).ready(function(){
+  				$('.shopping-cart-btn').popover({
+  					html: true,
+  					content: $('.shopping-cart-content').html()
+  				});
+
+  			});
+		</script>
+  		<link rel="stylesheet" href="/styles/styles.css">
+		<title>Awsome Webstore</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+		  <div class="navigation">
+		    <div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			     	<span class="icon-bar"></span>                        
+			    </button>
+		      	<a class="navbar-brand" href="/">Awsome Webstore</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      	<ul class="nav navbar-nav">
+		      		<li class="{{activeClass('home')}}"><a href="/">Home</a></li>
+		      		@foreach($navCategories as $category)
+						<li class="{{activeClass($category)}}"><a href="/category/{{$category->name}}">{{$category->displayname}}</a></li>
+		      		@endforeach
+		      	</ul>
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li><a href="#" class="shopping-cart-btn" data-toggle="popover" data-placement="bottom" title="Items in your shopping cart" ><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+			    </ul>
+		    </div>
+		  </div>
+		</nav>
+
+		<div class="container-fluid">
+			<div class="col-md-8 center-block">
+				@yield('content')
+			</div>
+		</div>
+
+		<!--
+		<footer class="container-fluid bg-4 text-center">
+			<p>Online store</p>
+		</footer>
+	-->
+
+	<div class="shopping-cart-content" style="display:none;">
+		@include("product.shopping_cart")
+	</div>
+	</body>
+</html>
